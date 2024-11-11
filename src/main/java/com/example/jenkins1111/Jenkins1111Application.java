b4bd163855ac212c582b18cba8b1b9b84cd57965
hyosoek <tennfin1@gmail.com>
2024-11-11 09:44:44 +0900

    init

---
+++ src/main/java/com/example/jenkins1111/Jenkins1111Application.java
@@ -0,0 +1,13 @@
+package com.example.jenkins1111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkins1111Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jenkins1111Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jenkins1111/Jenkins1111ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkins1111;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins1111ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
